2e41986cd4198d233d96ff93591634b2b4012f08
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-15 02:24:33 -0400

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1=2;
+const num2=31;
 
-// Write your code here
+const multiply=Number(num1 * num2);
+console.log(multiply);
+
+
+let random =42;
+console.log (Number.isInteger(random));
+
+
+const num3=14;
+const num4=5;
+
+const mod= Number(num3%num4);
+console.log (mod)
+
+
+const sets=[2,4,6,8,12,16,20];
+
+const max=Math.max(...sets);
+
+console.log (max);
